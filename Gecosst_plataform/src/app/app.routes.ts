5d4cad36fa3ac38ca0e0b506f9a0d15efd63e95b
zack2141 <wilmar.cardona7168@ucaldas.edu.co>
2026-07-01 06:13:34 -0500

    Construcción de componentes en el front

--- Gecosst_plataform/src/app/app.routes.ts
+++ Gecosst_plataform/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { InicioComponent } from './inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'', redirectTo:'inicio', pathMatch: 'full'},
+    {path:'inicio', component:InicioComponent},
+];
